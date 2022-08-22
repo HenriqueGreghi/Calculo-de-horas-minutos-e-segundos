6f086d9812282285c23015914565d0e2ad7b6182
Henrique Greghi <henriquevsg@gmail.com>
2022-08-22 10:36:54 -0300

    Calculo de segundos

---
+++ CalculoSegundos/Calculo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculoSegundos
+{
+    class Calculo
+    {
+        public int horas;
+        public int minutos;
+        public int segundos;
+        public int seg;
+
+        public void Calcular()
+        {
+            horas = seg / 3600;
+            minutos = (seg % 3600) / 60;
+            segundos = (seg % 3600) % 60;
+
+
+        }
+        public void Imprimir()
+        {
+            Console.WriteLine( + horas + ":" + minutos + ":" + segundos);
+        }
+    }
+}
---
+++ CalculoSegundos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CalculoSegundos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculo calc = new Calculo();
+
+            Console.WriteLine("Entre com os segundos: ");
+            calc.seg = int.Parse(Console.ReadLine());
+
+            calc.Calcular();
+            calc.Imprimir();
+        }
+    }
+}
